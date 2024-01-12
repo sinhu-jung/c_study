48609161f129f18ec5eeee7292451e3b163222ec
sinhu <tlsgn26@naver.com>
2024-01-12 18:34:31 +0900

    배열 최댓값 최솟값

---
+++ arrayMax/arrayMax.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int arr[5] = { 0 }, max = 0;
+
+	for (int i = 0; i < _countof(arr); i++) 
+		scanf_s("%d", &arr[i]);
+
+	// prob1
+	//for (int i = 0; i < _countof(arr); i++) 
+	//	max = max > arr[i] ? max : arr[i];
+
+	//printf("Max: %d", max);
+
+	// prob2
+	for (int i = 0; i < _countof(arr); i++) {
+		for (int j = 0; j < _countof(arr); j++) {
+			int s = 0;
+			if (arr[i] < arr[j]) {
+				s = arr[i];
+				arr[i] = arr[j];
+				arr[j] = s;
+			}
+		}
+	}
+
+	printf("Min: %d", arr[0]);
+	return 0;
+}
