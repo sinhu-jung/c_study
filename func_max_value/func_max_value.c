69a64bfe241d46d120ae46e0580d449718eadaf6
sinhu <tlsgn26@naver.com>
2024-01-15 12:23:40 +0900

    사용자 정의 함수 실습

---
+++ func_max_value/func_max_value.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int * getValue() {
+	static int a[3] = { 0 };
+	printf("정수 3개를 입력 하세요: ");
+	for (int i = 0; i < 3; i++) {
+		scanf_s("%d", &a[i]);
+	}
+	return a;
+}
+
+int getMax(int *value) {
+	int max = 0;
+	for (int i = 0; i < 3; i++) {
+		max = value[i] > max ? value[i] : max;
+	}
+	return max;
+}
+
+int main(void) {
+	int *value = getValue();
+	int max =  getMax(value);
+	printf("최댓값: %d\n", max);
+	return 0;
+}
