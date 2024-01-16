74e8429adcc4c34ac2e6619a194211268a2b77f1
sinhu <tlsgn26@naver.com>
2024-01-16 12:09:26 +0900

    문자열 깊은 복사

--- MemoryCopy/MemoryCopy.c
+++ MemoryCopy/MemoryCopy.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(void) {
 	char szBuffer[12] = { "HelloWord" };
 	char szNewBuffer[12] = { 0 };
 
+	char* a = malloc(12);
+
+	// string deep copy
+	strcpy_s(a, 12, szBuffer);
+	puts(a);
+	free(a);
 	// memcpy는 메모리 복사하는 함수 배열 복사시 사용
 	memcpy(szNewBuffer, szBuffer, 4);
 	puts(szNewBuffer);
